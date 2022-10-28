820de0dec62ff61a164bc6c2cb87a2458e05a75d
KELVIN JOSE PERALTA ALMONTE <108644732+KelvinPeralta@users.noreply.github.com>
2022-10-27 21:50:11 -0500

    Update SimilarProducts.jsx

--- src/components/ProductId/SimilarProducts.jsx
+++ src/components/ProductId/SimilarProducts.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import CardProduct from '../home/CardProduct'
+import CardProduct from '../home/cardProduct'
 
 
 const SimilarProducts = ({product}) => {
